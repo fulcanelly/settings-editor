d1b16290c288955ad074cce244ea95b0961a501f
fulcanelly <dallasxfulcanelly@gmail.com>
2021-12-11 02:47:49 +0200

    remake in more functional fashion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 class SettingsList extends Component {
   render() {
@@ -15,46 +15,39 @@ class SettingsList extends Component {
 }
 
 
-class App extends Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      version: 0
-    } 
-    this.handleSelect = this.handleSelect.bind(this)
-  }
-
-  handleSelect() {
-    var version = document.getElementById('version-selector').value
-    this.setState({ version: version });
-  }
-
-  render() {
-    return <div className="App">
-      <header className="App-header">
-        <p>
-          Select version
-        </p>
-        
-        <select 
-          class = "dropdown"
-          id='version-selector'
-          onChange ={this.handleSelect}
-          name="cars" >
-          <option value={0}>beyond 1.3</option>
-          <option value={1.3}>1.3</option>
-          <option value={1.5}>1.5</option>
-          
-        </select>
-
-        <p>
-          Settings generator for {this.state.version}
-        </p>
-        <SettingsList items = {[]}/>
-      </header>
-    </div>
-  }
+function handleSelect(setVersion) {
+  setVersion(
+    document.getElementById('version-selector').value
+  )
 }
+
+
+function App() {
+  const [version, setVersion] = useState(0)
+  return <div className="App">
+    <header className="App-header">
+      <p>
+        Select version
+      </p>
+      
+      <select 
+        class = "dropdown"
+        id='version-selector'
+        onChange = {() => handleSelect(setVersion)}
+        name="cars" >
+        <option value={0}>beyond 1.3</option>
+        <option value={1.3}>1.3</option>
+        <option value={1.5}>1.5</option>
+        
+      </select>
+
+      <p>
+        Settings generator for {version}
+      </p>
+      <SettingsList items = {[]}/>
+    </header>
+  </div>
+}
+
 
 export default App;
